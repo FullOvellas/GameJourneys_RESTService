cfbbc31b975df7bdaf970dcf87f6aee7c62d37d3
FullOvellas <sergiotui1c@gmail.com>
2023-06-08 22:51:16 +0200

    WIP dataset

--- db/series_data.sql
+++ db/series_data.sql
@@ -1,3 +1,4 @@
 INSERT INTO series (series_name) VALUES ('The Legend of Zelda');
 INSERT INTO series (series_name) VALUES ('Dark Souls');
 INSERT INTO series (series_name) VALUES ('Armored Core');
+INSERT INTO series (series_name) VALUES ('Final Fantasy');
